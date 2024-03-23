251f578f69f132596e9f711d27c6c335868deb5c
teenmaster <ambagwaeugene09@gmail.com>
2024-03-23 22:45:44 +0300

    Implemented one regex, and validate button with JS

--- telephone-number-validator/script.js
+++ telephone-number-validator/script.js
@@ -1,22 +1,26 @@
-const telephoneNumber = document.getElementById("user-input");
+const userInput = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const displayResult = document.getElementById('results-div');
 
-const validateTelephoneNumber = input => {
-  if (input === ""){
+const firstNumber = /^\(\d{3}\) \d{3}-\d{4}$/;
+
+const telephoneNumbers = [];
+
+const isTrueNumber = (number) => telephoneNumbers.some((regex) => regex.test(number));
+
+checkBtn.addEventListener("click", () => {
+  if (userInput.value === "") {
     alert("Please provide a phone number");
     return;
   }
-} 
-
-checkBtn.addEventListener("click", () => {
-  validateTelephoneNumber(telephoneNumber.value);
-  telephoneNumber.value = "";
+  displayResult.textContent = isTrueNumber(userInput.value) ? `Valid US number: ${userNumber.value}` : `Invalid US number: ${userNumber.value}`;
+  userInput.value = "";
 });
 
 //clear Button functionality
   clearBtn.addEventListener("click", () => {
-    telephoneNumber.value = "";
+    userInput.value = "";
     displayResult.innerHTML = "";
   });
+

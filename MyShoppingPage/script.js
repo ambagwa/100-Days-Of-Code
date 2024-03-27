108a9c9626e51c5aa68a990aed96f4d632feadd2
teenmaster <ambagwaeugene09@gmail.com>
2024-03-28 02:53:43 +0300

    Created an array of objects

---
+++ MyShoppingPage/script.js
@@ -0,0 +1,66 @@
+const cartContainer = document.getElementById("cart-container");
+const cartButton = document.getElementById("cart-btn");
+const showHideCart = document.getElementById("show-hide-cart");
+
+const products = [
+    {
+        id: 1,
+        name: "AF 1",
+        price: 15.99,
+        category: "Nike"
+    },
+    {
+        id: 2,
+        name: "SB dunks",
+        price: 25.99,
+        category: "Nike"
+    },
+    {
+        id: 3,
+        name: "Yeezy",
+        price: 35.99,
+        category: "Adidas"
+    },
+    {
+        id: 4,
+        name: "Converse",
+        price: 7.99,
+        category: "Vans"
+    },
+    {
+        id: 5,
+        name: "Low riders",
+        price: 37.99,
+        category: "Puma"
+    },
+    {
+        id: 6,
+        name: "Ngoma",
+        price: 9.29,
+        category: "Bata"
+    },
+    {
+        id: 7,
+        name: "Tops",
+        price: 19.91,
+        category: "New Balance"
+    },
+    {
+        id: 8,
+        name: "J1s",
+        price: 44.98,
+        category: "Nike"
+    },
+    {
+        id: 9,
+        name: "Flip flops",
+        price: 2.79,
+        category: "Umoja"
+    },
+    {
+        id: 6,
+        name: "Okala",
+        price: 4.99,
+        category: "Rubbers"
+    }
+];

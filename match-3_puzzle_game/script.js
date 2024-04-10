2e8b68750045cc43334ba5f437c7e2c9e5cab441
teenmaster <ambagwaeugene09@gmail.com>
2024-04-10 09:28:58 +0300

    Created a class for initializing a board

--- match-3_puzzle_game/script.js
+++ match-3_puzzle_game/script.js
@@ -1,20 +1,12 @@
-
-
+//get html elements
 const startBtn = document.getElementById("start-game");
 const canvas = document.getElementById("canvas");
 const startScreen = document.getElementById("start-screen");
 const ctx = canvas.getContext("2d");
 
-//size of the game board
-const numRows = 5;
-const numCols = 5;
-//the size of each cell
-const cellSize = 100;
-
-
 //set the size of the canvas
-canvas.width = numCols * cellSize;
-canvas.height = numRows * cellSize; 
+canvas.width = innerWidth;
+canvas.height = innerHeight; 
 
 //Create a class to represent gem objects in grid cells
 class Gem {
@@ -25,27 +17,41 @@ class Gem {
     }    
 }
 
-//function to generate a random color for the game
-const generateRandomColor = () => {
+
+class Board {
+    constructor (numRows, numCols, cellSize){
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.cellSize = cellSize;
+        this.gameBoardGrid = this.createGrid();
+    }
+
+    //Create a method for creating the grid
+    createGrid(){
+        //Represent the gameboard in a 2D array. Each element in the array 
+        //represents a row and each nested array represents the cells in 
+        //that row. Each cell contains info aabout a gem, ie color & pstn
+        const gameBoardGrid = [];
+        //initialize the grid that will be used as the game board
+        for (let i = 0; i < this.numRows; i++){
+            gameBoardGrid[i] = [];
+            for (let j = 0; j < this.numCols; j++){
+                //Generate a random color for the gem
+                const randomColor = this.generateRandomColor();
+                gameBoardGrid[i][j] = new Gem(randomColor, i, j);
+            }
+        }
+        return gameBoardGrid;
+    }
+
+    //method to generate a random color for the game
+    generateRandomColor(){
     const colors = ["red", "orange", "yellow", "green", "blue"];
     return colors[Math.floor(Math.random() * colors.length)];
 };
-
-
-//Represent the gameboard in a 2D array. Each element in the array 
-//represents a row and each nested array represents the cells in that row
-//each cell contais info aabout a gem
-const gameBoardGrid = [];
-
-//initialize the grid that will be used as the game board
-for (let i = 0; i < numRows; i++){
-    gameBoardGrid[i] = [];
-    for (let j = 0; j < numCols; j++){
-        //Generate a random color for the gem
-        const randomColor = generateRandomColor();
-        gameBoardGrid[i][j] = new Gem(randomColor, i, j);
-    }
 }
+
+const gameBoard = new Board(5, 5, 100);
 
 
 //Showcasing the game board on canvas means iterating over each cell in the 
@@ -54,24 +60,26 @@ const drawGameBoard = () => {
     //clear the caanvas before drawing
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    
+    //Calculate the height and width of the gameBoard area
+    const gameBoardHeight = gameBoard.numCols * gameBoard.cellSize;
+    const gameBoardWidth = gameBoard.numRows * gameBoard.cellSize;
 
-    for (let i = 0; i < numRows; i++){
-        for (let j = 0; j < numCols; j++){
+    for (let i = 0; i < gameBoard.numRows; i++){
+        for (let j = 0; j < gameBoard.numCols; j++){
             //get the gem at the current cell
-            const gem = gameBoardGrid[i][j];
+            const gem = gameBoard.gameBoardGrid[i][j];
             //calculate the x co-ordinates of the cell on the canvas
-            const x = j * cellSize;
+            const x = j * gameBoard.cellSize;
             //calculate the y co-ordinates of the cell on the canvas
-            const y = i * cellSize;
+            const y = i * gameBoard.cellSize;
             if (gem){
                 ctx.fillStyle = gem.color;
-                ctx.fillRect(x, y, cellSize, cellSize);
+                ctx.fillRect(x, y, gameBoard.cellSize, gameBoard.cellSize);
             }
             else{
                 //draw an empty cell if there's no gem
                 ctx.fillStyle = 'white';
-                ctx.fillRect(x, y, cellSize, cellSize);
+                ctx.fillRect(x, y, gameBoard.cellSize, gameBoard.cellSize);
             }
         }
     }
@@ -81,20 +89,20 @@ const drawGameBoard = () => {
     ctx.lineWidth = 1;//width of the grid lines
 
     //Draw horizontal grid lines
-    for (let j = 0; j < numRows; j++){
-        const y = j * cellSize;
+    for (let j = 0; j < gameBoard.numRows; j++){
+        const y = j * gameBoard.cellSize;
         ctx.beginPath();
         ctx.moveTo(0, y);
-        ctx.lineTo(canvas.width, y);
+        ctx.lineTo(gameBoardWidth, y);
         ctx.stroke();
     }
 
     //Draw vertical grid lines
-    for (let i = 0; i < numCols; i++){
-        const x = i * cellSize;
+    for (let i = 0; i < gameBoard.numCols; i++){
+        const x = i * gameBoard.cellSize;
         ctx.beginPath();
         ctx.moveTo(x, 0);
-        ctx.lineTo(x, canvas.height);
+        ctx.lineTo(x, gameBoardHeight);
         ctx.stroke();
     }
 

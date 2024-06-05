f2bcc5fced82fc049446df2de4bdf3dd71d5ad60
ambagwa <ambagwaeugene09@gmail.com>
2024-06-06 02:03:46 +0300

    Initial commit

---
+++ MyMoviesRetrievalApp/script.js
@@ -0,0 +1,21 @@
+const apiKey = "1c5a17c1";
+const title = "Antman";
+const apiUrl = `http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`;
+
+fetch(apiUrl)
+  .then(resolve => {
+    if (!resolve.ok) throw new Error("Poor network response");
+
+    return resolve.json();
+  })
+  .then(data => {
+    //check if the returned data is false, indicating an error from api
+    if ( data.Response === "False") throw new Error(data.Error);
+
+    console.log(data);
+  })
+  .catch("There has been a problem with your fetch operation");
+
+  //https://www.omdbapi.com/?s=batman&apikey=1c5a17c1
+
+  //const apiUrlPoster = ;

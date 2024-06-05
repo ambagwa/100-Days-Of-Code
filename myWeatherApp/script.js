2b1f4f2e7260f3424dd6163a77feb7616060488e
ambagwa <ambagwaeugene09@gmail.com>
2024-06-05 20:12:18 +0300

    Added more data to html

--- myWeatherApp/script.js
+++ myWeatherApp/script.js
@@ -30,34 +30,40 @@ fetch(apiUrl)
     .then(data => {
         //Extract the city name from the json
         const cityName = data.city.name;
+        //Extract country code
+        const countryCode = data.city.country;
         //Set the cityWeatherArr to the list of weather data
         cityWeatherArr = data.list;
         //populate the page with data
-        displayWeatherDetails(cityWeatherArr, cityName);
+        displayWeatherDetails(cityWeatherArr, cityName, countryCode);
     })
     .catch(err => console.error(`There was an error: ${err}`));
 
 
 
 //Populate the UI with weather data
-const displayWeatherDetails = (citiesWeatherList, cityName) => {
+const displayWeatherDetails = (citiesWeatherList, cityName, countryCode) => {
     if (citiesWeatherList && citiesWeatherList.length > 0) {
         //Extract weather details
         const { dt_txt, main: { temp }, weather: [{ description, icon }],
             wind: { speed }} = citiesWeatherList[0];
+        
+        //Variable for holding both cityName and countryCode
+        const cityInfo = `${cityName}, ${countryCode}`;
 
         //Display the extracted weather data onto html
         cityContainer.innerHTML += `
             <div class="weather-card">
-                <h2 class="city-name"><strong>${cityName}</strong></h2>
-                <p class="dt-txt description">${dt_txt}</p>
+                <h2 class="city-name"><strong>${cityInfo}</strong></h2>
                 <img class="weather-icon" 
                     src="http://openweathermap.org/img/wn/${icon}.png" 
                     alt="${description}"
                 />
-                <p class="description">${description}</p>
-                <p class="temperature">Temp: ${temp} F</p>
-                <p class="wind-speed">Wind speed: ${speed} m/s</p>
+                <p class="date-text">Forecast at:</p>
+                <p class="dt-txt">${dt_txt}</p>
+                <p class="description"><strong>Sky:</strong> ${description}</p>
+                <p class="temperature"><strong>Temp:</strong> ${temp} F</p>
+                <p class="wind-speed"><strong>Wind speed:</strong> ${speed} m/s</p>
             </div>
         `;
     };

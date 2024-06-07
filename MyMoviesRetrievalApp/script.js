0eb10a2ed70bf44e7fe9b72f05640a1971ebf460
ambagwa <ambagwaeugene09@gmail.com>
2024-06-07 03:11:46 +0300

    Toggle func for navbar

--- MyMoviesRetrievalApp/script.js
+++ MyMoviesRetrievalApp/script.js
@@ -1,6 +1,9 @@
+const movieContainer = document.getElementById("movie-container");
+
 const apiKey = "1c5a17c1";
-const title = "Prison+break";
-const apiUrl = `http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`;
+const titleQuery = "Prison+break";
+const titleSpecific = "Antman";
+const apiUrl = `http://www.omdbapi.com/?s=${titleQuery}&apikey=${apiKey}`;
 //const apiUrlPoster = `http://img.omdbapi.com/?s=${title}&apikey=${apiKey}`;
 
 fetch(apiUrl)
@@ -20,3 +23,12 @@ fetch(apiUrl)
   //https://www.omdbapi.com/?s=batman&apikey=1c5a17c1
 
   //http://img.omdbapi.com/?s=batman&apikey=$1c5a17c1
+
+  const toggleNav = () => {
+    const nav = document.getElementById("nav");
+    if (nav.style.width === "250px"){
+        nav.style.width = "0";
+    } else {
+        nav.style.width = "250px";
+    }
+  };

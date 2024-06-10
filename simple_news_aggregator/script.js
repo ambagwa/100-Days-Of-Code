c8aaf234127cb229953f072184655306ab9a166c
ambagwa <ambagwaeugene09@gmail.com>
2024-06-11 02:49:06 +0300

    try...catch

--- simple_news_aggregator/script.js
+++ simple_news_aggregator/script.js
@@ -1,21 +1,45 @@
-const apiKey = 'ea302295a96940d99b4602a8c480c5da'; // Replace with your NewsAPI key
-const searchButton = document.getElementById('searchButton');
-const searchInput = document.getElementById('searchInput');
-const newsContainer = document.getElementById('newsContainer');
-const apiUrl = `https://newsapi.org/v2/everything?q=rain+in+kenya&apiKey=${apiKey}`;
-let dataArr = [];
+const searchButton = document.getElementById("searchButton");
 
-fetch(apiUrl)
-    .then(res => {
-        if (!res.ok) {
-            throw new Error("Network error: ", res.statusText);
+searchButton.addEventListener("click", () => {
+  const apiKey = "ea302295a96940d99b4602a8c480c5da"; // Replace with your NewsAPI key
+  const searchInput = document.getElementById("searchInput").value.trim();
+  const newsContainer = document.getElementById("newsContainer");
+  let dataArr = [];
+  const apiUrl = `https://newsapi.org/v2/everything?q=${searchInput}&apiKey=${apiKey}`;
+
+    if(!searchInput) {
+        alert("Please enter a search term.");
+        return;
+    }
+
+    const fetchData = async () => {
+        try {
+            const resolve = await fetch(apiUrl);
+            const data = await resolve.json();
+            //Display the data
+            newsContainer.innerHTML = JSON.stringify(data, null, 2);
+        } catch(error){
+            newsContainer.innerHTML = error;
         }
-        return res.json();
+    };
+
+    fetchData();
+
+  /*
+  fetch(apiUrl)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network error: ", res.statusText);
+      }
+      return res.json();
     })
-    .then(data => {
-        dataArr = data;
-        newsContainer.innerHTML = JSON.stringify(data, null, 2);
+    .then((data) => {
+      //Get the array of articles
+      dataArr = data.articles;
+      newsContainer.innerHTML = JSON.stringify(data, null, 2);
     })
-    .catch(error => {
-        newsContainer.innerHTML = `Error: ${error.message}`;
+    .catch((error) => {
+      newsContainer.innerHTML = `Error: ${error.message}`;
     });
+    */
+});

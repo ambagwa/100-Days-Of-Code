cdc25527c4b2b82d2204f254e09e1c857353099b
ambagwa <ambagwaeugene09@gmail.com>
2024-06-12 02:14:12 +0300

    Displayed news onto html

--- simple_news_aggregator/script.js
+++ simple_news_aggregator/script.js
@@ -17,13 +17,46 @@ searchButton.addEventListener("click", () => {
             const resolve = await fetch(apiUrl);
             const data = await resolve.json();
             //Display the data
-            newsContainer.innerHTML = JSON.stringify(data, null, 2);
+            //newsContainer.innerHTML = JSON.stringify(data, null, 2);
+            displayResults(data);
+
         } catch(error){
             newsContainer.innerHTML = error;
         }
     };
 
     fetchData();
+
+    const displayResults = data => {
+      //Deconstruct specific propertirs from the returned json data
+      const { articles } = data;
+
+      //Contents of articles: author, content, description, publishedat
+      //source(name0), title, urltoImage, url
+      newsContainer.innerHTML += articles.map(item => {
+        const {
+          author, 
+          content, 
+          description, 
+          publishedAt,
+          source: { id, name},
+          title, 
+          url,
+          urlToImage
+        } = item;
+
+        return `
+          <div class="one-piece">
+            <h1 class="news-title">${title}</h1>
+            <img src="${urlToImage}" alt="news=image" class="news-image">
+            <p class="content">${content}</p>
+            <a target="_blank" href="${url}">Click here to see full news >>></a>
+            <p class="author">${author}</p>
+            <p class="date">${publishedAt}</p>
+          </div>
+        `;
+      });
+    };
 
   /*
   fetch(apiUrl)

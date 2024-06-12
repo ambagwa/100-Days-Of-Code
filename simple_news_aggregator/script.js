145af6430f3161fd2e14add634183f8b9a66226c
ambagwa <ambagwaeugene09@gmail.com>
2024-06-12 03:44:19 +0300

    Added moreBtn functionality

--- simple_news_aggregator/script.js
+++ simple_news_aggregator/script.js
@@ -1,51 +1,62 @@
 const searchButton = document.getElementById("searchButton");
+const moreBtn = document.getElementById("more-btn");
+
+moreBtn.style.display = "none";
 
 searchButton.addEventListener("click", () => {
   const apiKey = "ea302295a96940d99b4602a8c480c5da"; // Replace with your NewsAPI key
   const searchInput = document.getElementById("searchInput").value.trim();
   const newsContainer = document.getElementById("newsContainer");
   let dataArr = [];
+  let startingIndex = 0;
+  let endingIndex = 6;
   const apiUrl = `https://newsapi.org/v2/everything?q=${searchInput}&apiKey=${apiKey}`;
 
-    if(!searchInput) {
-        alert("Please enter a search term.");
-        return;
+  if (!searchInput) {
+    alert("Please enter a search term.");
+    return;
+  }
+
+  const fetchData = async () => {
+    try {
+      const resolve = await fetch(apiUrl);
+      const data = await resolve.json();
+
+      dataArr = data.articles;
+      //Display the data
+      //newsContainer.innerHTML = JSON.stringify(data, null, 2);
+      displayResults();
+      
+      moreBtn.style.display = "block";
+
+    } catch (error) {
+      newsContainer.innerHTML = error;
     }
+  };
 
-    const fetchData = async () => {
-        try {
-            const resolve = await fetch(apiUrl);
-            const data = await resolve.json();
-            //Display the data
-            //newsContainer.innerHTML = JSON.stringify(data, null, 2);
-            displayResults(data);
+  fetchData();
 
-        } catch(error){
-            newsContainer.innerHTML = error;
-        }
-    };
+  const displayResults = () => {
+    const slicedData = dataArr.slice(
+      startingIndex,
+      startingIndex + endingIndex
+    );
 
-    fetchData();
+    //Contents of articles: author, content, description, publishedat
+    //source(name0), title, urltoImage, url
+    newsContainer.innerHTML += slicedData.map((item) => {
+      const {
+        author,
+        content,
+        description,
+        publishedAt,
+        source: { id, name },
+        title,
+        url,
+        urlToImage,
+      } = item;
 
-    const displayResults = data => {
-      //Deconstruct specific propertirs from the returned json data
-      const { articles } = data;
-
-      //Contents of articles: author, content, description, publishedat
-      //source(name0), title, urltoImage, url
-      newsContainer.innerHTML = articles.map(item => {
-        const {
-          author, 
-          content, 
-          description, 
-          publishedAt,
-          source: { id, name},
-          title, 
-          url,
-          urlToImage
-        } = item;
-
-        return `
+      return `
           <div class="one-piece">
             <h1 class="news-title">${title}</h1>
             <img src="${urlToImage}" alt="news=image" class="news-image">
@@ -56,8 +67,17 @@ searchButton.addEventListener("click", () => {
             <div class="bottom-div"></div>
           </div>
         `;
-      });
-    };
+    }).join("");
+    startingIndex += endingIndex;
+
+    if (startingIndex >= endingIndex) {
+      moreBtn.style.display = "none";
+    }
+  };
+
+  moreBtn.addEventListener("click", () => {
+    displayResults();
+  });
 
   /*
   fetch(apiUrl)

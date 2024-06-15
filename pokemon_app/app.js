5042de09056a5a15ba602c37bbb462bfc37cf768
ambagwa <ambagwaeugene09@gmail.com>
2024-06-15 03:04:09 +0300

    DOM dynamic creation

--- pokemon_app/app.js
+++ pokemon_app/app.js
@@ -1,24 +1,24 @@
-const nameOrId = document.getElementById("search-input").value;
 const searchBtn = document.getElementById("search-button");
-const apiUrl = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${nameOrId}`;
+const resultsDiv = document.getElementById("results");
 let dataArr = [];
 //console.log(apiUrl);
 
 searchBtn.addEventListener("click", () => {
-    if (!nameOrId) {
-        alert("Enter pokemon name or Id");
-    }
+  const nameOrId = document.getElementById("search-input").value;
+  const apiUrl = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${nameOrId}`;
+  if (!nameOrId) {
+    alert("Enter pokemon name or Id");
+    return;
+  }
 
   const fetchData = async () => {
     try {
       const resolve = await fetch(apiUrl);
       const data = await resolve.json();
 
-      dataArr = data;
-
-      console.log(data);
+      displayResults(data);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      resultsDiv.innerHTML(error);
     }
   };
 
@@ -27,8 +27,8 @@ searchBtn.addEventListener("click", () => {
   const displayResults = (data) => {
     //extract stats from the data object
     const stats = {};
-    data.stats.forEach(stat => {
-        stats[stat.stat.name] = stat.base_stat;
+    data.stats.forEach((stat) => {
+      stats[stat.stat.name] = stat.base_stat;
     });
 
     //Access the properties inside the stats object
@@ -40,10 +40,27 @@ searchBtn.addEventListener("click", () => {
     const specialDefense = stats["special-defense"];
 
     //Access other properties directly from the data object
-    const types= data.types.map(type => type.type.name).join(", ");
+    const types = data.types.map((type) => type.type.name).join(", ");
     const height = data.height;
     const weight = data.weight;
     const id = data.id;
     const name = data.name;
+    const sprite = data.sprites.front_default;
+
+    //upload DOM with the fetched data
+    resultsDiv.innerHTML += `
+        <img src="${sprite}" alt="pokemon image" id="sprite">
+        <div id="types">${types}</div>
+        <p id="pokemon_name">${name} </p>
+        <p id="pokemon_id">${id}</p>
+        <p id="weight">Pokemon weight: ${weight}</p>
+        <p id="height">pokemon height: ${height}</p>
+        <p id="hp">HP: ${hp}</p>
+        <p id="speed">Speed: ${speed}</p>
+        <p id="attack">Attack: ${attack}</p>
+        <p id="defense">Defense: ${defense}</p>
+        <p id="special_attack">Special attack: ${specialAttack}</p>
+        <p id="special_defense">Special defense: ${specialDefense}</p>
+    `;
   };
 });

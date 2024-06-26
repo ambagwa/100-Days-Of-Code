6983044ae72e383eaf014fcae8b22038058d1064
ambagwa <ambagwaeugene09@gmail.com>
2024-06-27 02:44:14 +0300

    First commit

---
+++ book_inventory_management/script.js
@@ -0,0 +1,30 @@
+const submitBtn = document.getElementById("submit-btn");
+
+const books = [
+    {
+        name: "The maze",
+        author: "Eugene Ambagwa", 
+        ISBN: 1100,
+        price: "$1547",
+        year: 2024,
+        "stock number": 1
+    },
+    {
+        name: "Life's out",
+        author: "Lee Dixon", 
+        ISBN: 1101,
+        price: "$547",
+        year: 1976,
+        "stock number": 2
+    },
+    {
+        name: "Kigogo",
+        author: "Pauline Kea", 
+        ISBN: 1102,
+        price: "$147",
+        year: 2018,
+        "stock number": 3
+    }
+];
+
+submitBtn.addEventListener("click", () => {});

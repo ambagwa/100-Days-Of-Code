68955127292dbb380e9da8b6299b84c2707f30e3
ambagwa <ambagwaeugene09@gmail.com>
2024-08-26 21:05:27 +0300

    portfolio section

--- model-portfolio/script.js
+++ model-portfolio/script.js
@@ -2,28 +2,29 @@ const imageContainer = document.getElementById("image-container");
 
 
 const imageUrls = [
-    {image: "https://images.pexels.com/photos/1921168/pexels-photo-1921168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {image: "https://images.pexels.com/photos/1921168/pexels-photo-1921168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {image: "https://images.pexels.com/photos/1921168/pexels-photo-1921168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {image: "https://images.pexels.com/photos/1921168/pexels-photo-1921168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {image: "https://images.pexels.com/photos/1921168/pexels-photo-1921168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {image: "https://images.pexels.com/photos/1921168/pexels-photo-1921168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {image: "https://images.pexels.com/photos/1921168/pexels-photo-1921168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {image: "https://images.pexels.com/photos/1921168/pexels-photo-1921168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-    {image: "https://images.pexels.com/photos/2112713/pexels-photo-2112713.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
-    {image: "https://images.pexels.com/photos/2168257/pexels-photo-2168257.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
-    {image: "https://images.pexels.com/photos/4100276/pexels-photo-4100276.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"},
-    {image: "https://images.pexels.com/photos/1549437/pexels-photo-1549437.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"},
-    {image: "https://images.pexels.com/photos/3190033/pexels-photo-3190033.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"},
-    {image: "https://images.pexels.com/photos/955413/pexels-photo-955413.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"},
-    {image: "https://images.pexels.com/photos/1868925/pexels-photo-1868925.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"},
-    {image: "https://images.pexels.com/photos/2661536/pexels-photo-2661536.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"}
+    {image: "images/image1.jpg"},
+    {image: "images/image2.jpg"},
+    {image: "images/image3.jpg"},
+    {image: "images/image4.jpg"},
+    {image: "images/image5.jpg"},
+    {image: "images/image6.jpg"},
+    {image: "images/image7.jpg"},
+    {image: "images/image8.jpg"},
+    {image: "images/image9.jpg"},
+    {image: "images/image10.jpg"},
+    {image: "images/image11.jpg"},
+    {image: "images/image12.jpg"},
+    {image: "images/image13.jpg"},
+    {image: "images/image14.jpg"},
+    {image: "images/image15.jpg"},
+    {image: "images/image16.jpg"},
+    {image: "images/image17.jpg"},
 ];
 
 imageUrls.forEach(({ image }) => {
     imageContainer.innerHTML += `
         <div class="col image">
-            <img src="${image}" class="object-fit-cover img-fluid">
+            <img src="${image}" class="object-fit-cover img-fluid w-100" style="border-radius: 5px;">
         </div>
     `;
 });
